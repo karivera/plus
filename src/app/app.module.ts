a0489a1edcee024b352177ca41ae65aa85a608b2
Fanny-Bello98 <69219572+Fanny-Bello98@users.noreply.github.com>
2020-08-12 15:58:21 -0500

    Cambio de Fanny

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { FannyComponent } from './components/Fanny/Fanny.component';
+
+const rutas: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+redirectTo: 'Fanny'
+  },
+  {
+    path: 'Fanny',
+    component: FannyComponent
+    
+  },
+]
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/fanny/fanny.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fanny',
+  templateUrl: './fanny.component.html',
+  styleUrls: ['./fanny.component.css']
+})
+export class FannyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
